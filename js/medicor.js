6cdff8db38e8de7f94645fe9734fdd7495263f06
medicor <roger@medicor.se>
2015-10-23 21:20:14 +0000

    Added this reference to visualize

--- js/medicor.js
+++ js/medicor.js
@@ -1,9 +1,11 @@
 /* Thanks to https://github.com/ragamufin/nodedump */
-function visualize(aURI) {
-    var xhr = new XMLHttpRequest();
+function visualize(anElement, aURI) {
+    var that = this,
+        xhr  = new XMLHttpRequest(),
+        ;
     xhr.onreadystatechange = function () {
         if (this.readyState === 4 && this.status == 200) {
-            this.parentNode.innerHTML = nodedump(JSON.parse(xhr.responseText));
+            that.parentNode.innerHTML = nodedump(JSON.parse(xhr.responseText));
         }
     }
     xhr.open('GET', aURI, true);
