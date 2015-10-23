6a7a3d79a48248dffcec2a827dededb56e22c22a
medicor <roger@medicor.se>
2015-10-23 21:21:24 +0000

    Fixed syntax errors

--- js/medicor.js
+++ js/medicor.js
@@ -1,13 +1,12 @@
 /* Thanks to https://github.com/ragamufin/nodedump */
-function visualize(anElement, aURI) {
+function visualize(aURI) {
     var that = this,
-        xhr  = new XMLHttpRequest(),
-        ;
+        xhr  = new XMLHttpRequest();
     xhr.onreadystatechange = function () {
         if (this.readyState === 4 && this.status == 200) {
             that.parentNode.innerHTML = nodedump(JSON.parse(xhr.responseText));
         }
-    }
+    };
     xhr.open('GET', aURI, true);
     xhr.send();
 }
