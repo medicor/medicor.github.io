7354f4fa569f18cbac7a1bd54990472947cbe8df
medicor <roger@medicor.se>
2015-10-24 20:21:12 +0000

    Added support for XDomainRequest in jsondump

--- js/medicor.js
+++ js/medicor.js
@@ -1,7 +1,7 @@
 /* Thanks to https://github.com/ragamufin/nodedump */
 function jsondump(anEvent, aURI) {
     var that = anEvent.parentNode,
-        xhr  = new XMLHttpRequest();
+        xhr  = window.XDomainRequest ? new XDomainRequest() : new XMLHttpRequest();
     xhr.onreadystatechange = function () {
         if (this.readyState === 4 && this.status == 200) {
             that.innerHTML = nodedump(JSON.parse(xhr.responseText));
